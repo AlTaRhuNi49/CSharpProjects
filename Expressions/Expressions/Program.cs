aada7b0ceca7a22bb9df515e4b9dce8ce63365fa
AlTaRhuNi49 <proforall49@gmail.com>
2016-10-22 19:35:52 +0800

    expressions_C#
    explains expressions in c sharp

---
+++ Expressions/Expressions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float myVar = 10.5f;
+            float myVar2 = 10.4f;
+            float temp = myVar + myVar2;
+
+            //basic operators: (+ - / *)
+            //bitwise operations 
+            //logical operators: (Boolean true and false) 
+
+            //1- uniray operand
+            //2- binary operands: most of operators
+            //3- 3 operands: has one which is conditional operator
+        }
+    }
+}
