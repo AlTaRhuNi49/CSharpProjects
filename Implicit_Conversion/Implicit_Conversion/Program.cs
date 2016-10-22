169d4e339a8b49b9aa5221865ac3f9f961bba0c6
AlTaRhuNi49 <proforall49@gmail.com>
2016-10-23 03:47:14 +0800

    Implicit_Conversion_C#

---
+++ Implicit_Conversion/Implicit_Conversion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implicit_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long var = 299;
+            float var1 = var;
+            Console.WriteLine(var1);
+
+            char var2 = 'G';
+            int var3 = var2;
+            Console.WriteLine(var3);
+
+            Console.ReadKey();
+        }
+    }
+}
