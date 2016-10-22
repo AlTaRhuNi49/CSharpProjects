d363f02a7b1bc8dc559ead9cab30ed3460432ce5
AlTaRhuNi49 <proforall49@gmail.com>
2016-10-23 02:09:19 +0800

    Break_Continue_C#

---
+++ break_continue/break_continue/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace break_continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10, r = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                r += n;
+                Console.WriteLine(r);
+                if (i > 6)
+                    break;
+                if (i>3)
+                continue;
+                
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.WriteLine("inside loop");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
