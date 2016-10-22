b249227b344509ed0bfaf7ae1ec6b7419684859a
AlTaRhuNi49 <proforall49@gmail.com>
2016-10-22 23:18:50 +0800

    add boolean conditions

--- Boolean_Operators/Boolean_Operators/Program.cs
+++ Boolean_Operators/Boolean_Operators/Program.cs
@@ -14,9 +14,13 @@ namespace Boolean_Operators
             //Boolean Condition &&, ||
 
             bool myResult;
+            bool myResult2;
             int myVar1 = 5, myVar2 = 6;
             myResult = myVar1 < myVar2;
             Console.WriteLine(myResult);
+
+            myResult2 = (myVar1 == 5) && (myVar2 == 6);
+            Console.WriteLine(myResult2);
             Console.ReadKey();
         }
     }
