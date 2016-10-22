ab1cdde2ca93a300e401a2a6e0414d199098fa39
AlTaRhuNi49 <proforall49@gmail.com>
2016-10-22 21:13:50 +0800

    Increments_Decrements_C#
    explain increments and decrements operators in C#

---
+++ Increments_Decrements/Increments_Decrements/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Increments_Decrements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int var = 2;
+            int temp = ++var;
+            int temp2 = --var;
+            Console.WriteLine(temp);
+            Console.WriteLine(temp2);
+            Console.ReadKey();
+        }
+    }
+}
