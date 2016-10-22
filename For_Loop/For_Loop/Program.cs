bec8cd0021361db68ec00ba8222f150b377955c6
AlTaRhuNi49 <proforall49@gmail.com>
2016-10-23 01:43:26 +0800

    For_Loop_C#

---
+++ For_Loop/For_Loop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10, r = 0;
+            for(int i =0; i <10; i++)
+            {
+                r += n;
+                Console.WriteLine(r);
+            }
+            Console.ReadKey();
+        }
+    }
+}
