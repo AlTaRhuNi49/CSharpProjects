f5f167a26780f8c5071062cfdadeda2840cf6479
AlTaRhuNi49 <proforall49@gmail.com>
2016-10-23 00:22:55 +0800

    TernaryOperators_C#

---
+++ TernaryOperator/TernaryOperator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TernaryOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int var = 10;
+            //string result = (var < 9) ? "less than 9" : "more than 9";
+            //Console.WriteLine(result);
+            //Console.ReadKey();
+            //===============================
+
+            string myName = "meda", result;
+            result = (myName.Equals("meda")) ? "yes" : "no";
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
