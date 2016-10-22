5594dd4553d90d5f25432c57d1208b87965f3d62
AlTaRhuNi49 <proforall49@gmail.com>
2016-10-22 23:56:29 +0800

    if_Statement_C#
    explains if statement in c sharp

---
+++ If_Statement/If_Statement/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int var1 = 10;
+            if (var1 == 10)
+                Console.WriteLine("yes");
+            else
+                Console.WriteLine("no");
+            Console.ReadKey();
+
+
+        }
+    }
+}
