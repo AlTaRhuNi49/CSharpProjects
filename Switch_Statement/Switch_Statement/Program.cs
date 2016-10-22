de26ade08b15c85db5d4531e2a768813c706572a
AlTaRhuNi49 <proforall49@gmail.com>
2016-10-23 00:13:23 +0800

    Switch_Statement_C#

---
+++ Switch_Statement/Switch_Statement/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myAnimal = "cat";
+            switch (myAnimal)
+            {
+                case "dog":
+                    Console.WriteLine("this is dog");
+                    break;
+                case "bird":
+                    Console.WriteLine("this is bird");
+                    break;
+                case "cat":
+                    Console.WriteLine("this is cat");
+                    break;
+                default:
+                    Console.WriteLine("this is default");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
