f7292d0258b0ada2c9f10681c3f503fdba634c5d
AlTaRhuNi49 <proforall49@gmail.com>
2016-10-23 01:38:04 +0800

    While

---
+++ While/While/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10, r = 0, i = 0;
+            while(i<10)
+                {
+                r += n;
+                i++;
+            }
+            Console.WriteLine(r);
+            Console.ReadKey();
+        }
+    }
+}
