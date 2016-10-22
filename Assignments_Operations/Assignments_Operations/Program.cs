773e0f02c160428e86e5fd98a745c1d65ea1e169
AlTaRhuNi49 <proforall49@gmail.com>
2016-10-22 23:08:23 +0800

    Assignment_Operators_C#
    explains what is  Assignment_Operators

---
+++ Assignments_Operations/Assignments_Operations/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //=, +=, -=, *=, /=, %=
+
+            int myVar1 = 7, myVar2 = 8;
+
+            //myVar1 = myVar2;
+            //Console.WriteLine(myVar1);
+            //=====================
+            myVar1 += myVar2;
+            Console.WriteLine(myVar1);
+            Console.ReadKey();
+        }
+    }
+}
