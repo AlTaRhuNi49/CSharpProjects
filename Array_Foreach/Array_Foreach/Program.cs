7c01ec2947fb29875e16e9e1831966a68544936b
AlTaRhuNi49 <proforall49@gmail.com>
2016-10-24 00:36:30 +0800

    Array_Foreach_C#
    array with foreach in c sharp

---
+++ Array_Foreach/Array_Foreach/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myNumbers = new int[5] { 1, 2, 3, 4, 5 };
+            foreach(int items in myNumbers)
+            {
+                Console.WriteLine(items);
+            }
+            Console.ReadKey();
+        }
+    }
+}
