2ec3c38cb94879885a7f543b41d3a110243b6814
AlTaRhuNi49 <proforall49@gmail.com>
2016-10-23 16:00:22 +0800

    explicit_conversion_c#
    this may cause lose data if there is an overflow

---
+++ explicit_conversion/explicit_conversion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace explicit_conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte mySbyte;                                   //0 - 255
+            int myInt = 350;                                //000101011110 = 255
+            //mySbyte = myInt; //this is wrong              //000011111111 = 350
+            mySbyte = (byte)myInt;                          //000001011110 = 94
+            Console.WriteLine(mySbyte);
+            Console.ReadKey();
+
+
+        }
+    }
+}
