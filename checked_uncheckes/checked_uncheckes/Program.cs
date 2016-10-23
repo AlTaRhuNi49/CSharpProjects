c6254b645597fa609101607999e6686da0be3da9
AlTaRhuNi49 <proforall49@gmail.com>
2016-10-23 16:09:12 +0800

    checked_unchecked_c#
    checked used to check if there is any overflow
    unchecked used to leave overflow and continue implementing the code

---
+++ checked_uncheckes/checked_uncheckes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace checked_uncheckes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //checked use to check if there any overflow
+            //uncheck leave any overflow if there is
+
+            byte mySbyte;                                   //0 - 255
+            int myInt = 350;                                //000101011110 = 255
+            //mySbyte = myInt; //this is wrong              //000011111111 = 350
+            //mySbyte = checked((byte)myInt);                 //000001011110 = 94
+            mySbyte = unchecked((byte)myInt);
+            Console.WriteLine(mySbyte);
+            Console.ReadKey();
+        }
+    }
+}
