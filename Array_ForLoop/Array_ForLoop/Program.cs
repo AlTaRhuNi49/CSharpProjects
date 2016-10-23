eb0735d3d83abe2cc15af12c9d357c16bb117ad7
AlTaRhuNi49 <proforall49@gmail.com>
2016-10-24 00:17:34 +0800

    Array_ForLoop_C#
    array with for loop in c sharp

---
+++ Array_ForLoop/Array_ForLoop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myNumbers = { 1, 2, 3, 4, 5, 6 };//first way to declare the array
+            int[] myNumbers2 = new int[5];//second way to declare the array
+            int[] myNumber3 = new int[5] { 1, 2, 3, 4, 5};//third way to declare the array
+            Console.WriteLine(myNumbers[2]);
+
+            for(int i=0; i < 7; i++)
+            {
+                Console.WriteLine(myNumbers[i]);
+            }
+
+
+        }
+    }
+}
