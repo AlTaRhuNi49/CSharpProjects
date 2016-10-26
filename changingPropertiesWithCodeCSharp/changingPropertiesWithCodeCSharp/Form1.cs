b450d8d37404717baa0e265860f58bb07da76d29
AlTaRhuNi49 <proforall49@gmail.com>
2016-10-26 21:46:09 +0800

    textBox
    get text from text box to label using button

---
+++ changingPropertiesWithCodeCSharp/changingPropertiesWithCodeCSharp/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace changingPropertiesWithCodeCSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Text = "check";
+            label1.Height = 20;
+            label1.Width = 30;
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
